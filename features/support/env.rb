6a19756a8f8a69e2296de59708457d4f82ae1e8f
Numerico <webmaster@numerica.cl>
2013-10-13 22:19:50 -0300

    use dummy app for cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,10 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+# USE DUMMY APP FOR TESTS
+ENV["RAILS_ENV"] ||= "test"
+ENV["RAILS_ROOT"] ||= File.expand_path(File.dirname(__FILE__) + '/../../spec/dummy/')
+
 require 'cucumber/rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
